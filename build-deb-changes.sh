6e964fe171f2b5c788b9979fbd9abc9f2b651e5c
Xav Paice <xav.paice@canonical.com>
2018-11-22 10:01:38 +1300

    xenial release

--- build-deb-changes.sh
+++ build-deb-changes.sh
@@ -2,6 +2,13 @@
 
 # quick script to make a fresh changes file to upload to PPA
 
+
+# deps
+add-apt-repository ppa:canonical-bootstack/thruk
+apt-get update
+apt-get install libthruk debhelper lsb-release bc devscripts libfile-slurp-perl libdate-calc-perl libcss-compressor-perl libthruk libcpanel-json-xs-perl libmodule-install-perl
+
+
 ./configure --with-httpd-conf /etc/apache2/conf-available
 perl Makefile.PL
 make
